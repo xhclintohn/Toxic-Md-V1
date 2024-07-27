ef1176b24f402456f7c5e9a96d6c927163091acb
𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧 <152104512+xhclintohn@users.noreply.github.com>
2024-07-27 15:30:35 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,20 +6,20 @@ global.audio = "";
 global.video = "";
 global.port = process.env.PORT;
 global.appUrl = process.env.APP_URL || "";
-global.email = "xhclinton@gmail.com";
-global.location = "Kenya,Nairobi";
+global.email = "davidcyril209@gmail.com";
+global.location = "Abuja, Nigeria";
 global.mongodb = process.env.MONGODB_URI || "";
 global.allowJids = process.env.ALLOW_JID || "null";
 global.blockJids = process.env.BLOCK_JID || "null";
 global.DATABASE_URL = process.env.DATABASE_URL || "";
-global.timezone = process.env.TZ || process.env.TIME_ZONE || "Africa/Nair";
-global.github = process.env.GITHUB || "https://github.com/xhclintohn/Toxic-Md-V1";
-global.gurl = process.env.GURL || "https://chat.whatsapp.com/HuUiHp29GOK6UqFPAIwxHD";
-global.website = process.env.GURL || "https://github.com/xhclintohn";
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/16c27a981a3fb56409c26.jpg";
-global.devs = "https://t.me/xhclintohn";
+global.timezone = process.env.TZ || process.env.TIME_ZONE || "Africa/Lagos";
+global.github = process.env.GITHUB || "https://github.com/DeeCeeXxx/Queen_Anita-V2";
+global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029VaeRru3ADTOEKPCPom0L";
+global.website = process.env.GURL || "https://whatsapp.com/channel/0029VaeRru3ADTOEKPCPom0L";
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/17c8ba84a7761eed633f6.jpg";
+global.devs = "234906628353";
 global.sudo = process.env.SUDO || "";
-global.owner = process.env.OWNER_NUMBER || "";
+global.owner = process.env.OWNER_NUMBER || "2347043759577";
 global.style = process.env.STYLE || "3";
 global.gdbye = process.env.GOODBYE || "false";
 global.wlcm = process.env.WELCOME || "false";
@@ -48,11 +48,11 @@ module.exports = {
   HANDLERS: process.env.PREFIX || ".",
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "1.0.0",
-  caption: process.env.CAPTION || "`Toxic-V1™`",
-  author: process.env.PACK_AUTHER || "Toxic-V1",
-  packname: process.env.PACK_NAME || "Toxic",
-  botname: process.env.BOT_NAME || "Toxic-V1",
-  ownername: process.env.OWNER_NAME || "xhclinton",
+  caption: process.env.CAPTION || "`QUEEN_ANITA-V2™`",
+  author: process.env.PACK_AUTHER || "QUEEN_ANITA-V2",
+  packname: process.env.PACK_NAME || "A N I T A",
+  botname: process.env.BOT_NAME || "QUEEN_ANITA-V2",
+  ownername: process.env.OWNER_NAME || "David Cyril",
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
@@ -64,7 +64,7 @@ module.exports = {
   aitts_Voice_Id: process.env.AITTS_ID || "37",
   ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
   WORKTYPE: process.env.WORKTYPE || process.env.MODE || "public",
-  LANG: (process.env.THEME || "T O X I C").toUpperCase(),
+  LANG: (process.env.THEME || "A N I T A").toUpperCase(),
 };
 global.rank = "updated";
 global.isMongodb = false;
