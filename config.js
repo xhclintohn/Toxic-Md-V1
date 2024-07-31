0d1a215eeb15cbc634833857f88106660f3f3118
𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧 <152104512+xhclintohn@users.noreply.github.com>
2024-07-31 15:31:29 +0300

    tOXIC tECH

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
+
 //#ENJOY
 const fs = require("fs-extra");
 if (fs.existsSync(".env"))
@@ -7,19 +8,19 @@ global.video = "";
 global.port = process.env.PORT;
 global.appUrl = process.env.APP_URL || "";
 global.email = "xhclinton@gmail.com";
-global.location = "Abuja, Nairobi";
+global.location = "Nairobi, Kenya";
 global.mongodb = process.env.MONGODB_URI || "";
 global.allowJids = process.env.ALLOW_JID || "null";
 global.blockJids = process.env.BLOCK_JID || "null";
 global.DATABASE_URL = process.env.DATABASE_URL || "";
 global.timezone = process.env.TZ || process.env.TIME_ZONE || "Africa/Nairobi";
 global.github = process.env.GITHUB || "https://github.com/xhclintohn/Toxic-Md-V1";
-global.gurl = process.env.GURL || "https://chat.whatsapp.com/HuUiHp29GOK6UqFPAIwxHD";
-global.website = process.env.GURL || "https://chat.whatsapp.com/HuUiHp29GOK6UqFPAIwxHD";
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/8ab11a815c7c683d3bb91.jpg";
-global.devs = "254735342808";
+global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029VaeRru3ADTOEKPCPom0L";
+global.website = process.env.GURL || "https://whatsapp.com/channel/0029VaeRru3ADTOEKPCPom0L";
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/17c8ba84a7761eed633f6.jpg";
+global.devs = "https://t.me/xhclintohn";
 global.sudo = process.env.SUDO || "";
-global.owner = process.env.OWNER_NUMBER || "254735342808";
+global.owner = process.env.OWNER_NUMBER || "";
 global.style = process.env.STYLE || "3";
 global.gdbye = process.env.GOODBYE || "false";
 global.wlcm = process.env.WELCOME || "false";
@@ -27,7 +28,7 @@ global.warncount = process.env.WARN_COUNT || 3;
 global.disablepm = process.env.DISABLE_PM || "false";
 global.disablegroup = process.env.DISABLE_GROUPS || "false",
 global.MsgsInLog = process.env.MSGS_IN_LOG || "false";
-global.userImages = process.env.USER_IMAGES || "https://telegra.ph/file/8ab11a815c7c683d3bb91.jpg";
+global.userImages = process.env.USER_IMAGES || "https://i.imgur.com/H4qeXwa.jpeg,https://telegra.ph/file/ba9ced500f9eca7db8acb.mp4";
 global.waPresence = process.env.WAPRESENCE || "available";
 global.readcmds = process.env.READ_COMMAND || "false";
 global.readmessage = process.env.READ_MESSAGE || "false";
@@ -49,10 +50,10 @@ module.exports = {
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "1.0.0",
   caption: process.env.CAPTION || "`Toxic-Md-V1™`",
-  author: process.env.PACK_AUTHER || "Toxic-Md-V1",
-  packname: process.env.PACK_NAME || "TOXIC",
+  author: process.env.PACK_AUTHER || "Toxic-V1",
+  packname: process.env.PACK_NAME || "Toxic",
   botname: process.env.BOT_NAME || "Toxic-Md-V1",
-  ownername: process.env.OWNER_NAME || "xhclinton",
+  ownername: process.env.OWNER_NAME || "xh_clinton",
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
@@ -64,7 +65,7 @@ module.exports = {
   aitts_Voice_Id: process.env.AITTS_ID || "37",
   ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
   WORKTYPE: process.env.WORKTYPE || process.env.MODE || "public",
-  LANG: (process.env.THEME || "TOXIC").toUpperCase(),
+  LANG: (process.env.THEME || "Toxic").toUpperCase(),
 };
 global.rank = "updated";
 global.isMongodb = false;
